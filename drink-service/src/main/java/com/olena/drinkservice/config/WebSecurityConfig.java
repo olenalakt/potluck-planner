6c7ff3cd3fad99b344a09fe139924e675312e98a
Olena Laktyushkina <olaktyushkina@intercall.com>
2021-12-10 15:47:33 -0500

    Configured actuators in all services, exposed metrics for prometheus

---
+++ drink-service/src/main/java/com/olena/drinkservice/config/WebSecurityConfig.java
@@ -0,0 +1,62 @@
+package com.olena.guestservice.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+@Configuration
+@Slf4j
+public class WebSecurityConfig extends ResourceServerConfigurerAdapter {
+
+    @Autowired
+    CorsAndSecurityProperties corsAndSecurityProperties;
+
+    @Override
+    public void configure(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.anonymous().
+                and().authorizeRequests().antMatchers("/actuator/**").permitAll().
+                and().authorizeRequests().antMatchers(HttpMethod.OPTIONS, "/v1/guests/**").permitAll().
+                anyRequest().authenticated();
+
+        if (corsAndSecurityProperties.getContentSecurityPolicy() != null) {
+            httpSecurity.headers().contentSecurityPolicy(corsAndSecurityProperties.getContentSecurityPolicy());
+        }
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+
+        log.debug("corsConfigurationSource, entered: accessControlAllowCredentials=[{}], accessControlAllowOriginList=[{}]",
+                corsAndSecurityProperties.getAccessControlAllowCredentials(),
+                corsAndSecurityProperties.getAccessControlAllowOriginList());
+
+        RegexCorsConfiguration configuration = new RegexCorsConfiguration();
+        configuration.applyPermitDefaultValues();
+        List<String> accessControlAllowOrigin = corsAndSecurityProperties.getAccessControlAllowOriginList() == null ? Arrays.asList("*") : corsAndSecurityProperties.getAccessControlAllowOriginList();
+        Boolean accessControlAllowCredentials = corsAndSecurityProperties.getAccessControlAllowCredentials();
+
+        log.debug("corsConfigurationSource, set: accessControlAllowCredentials=[{}], accessControlAllowOriginList=[{}]",
+                accessControlAllowCredentials,
+                accessControlAllowOrigin);
+
+        configuration.setAllowedOriginRegex(accessControlAllowOrigin);
+        configuration.setAllowedMethods(Collections.singletonList("*"));
+        configuration.setAllowCredentials(accessControlAllowCredentials);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+
+    }
+}
