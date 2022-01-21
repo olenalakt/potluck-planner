5bc8bbece01f644be0c9492f6616adac7e4daf99
Olena Laktyushkina <olaktyushkina@intercall.com>
2022-01-21 15:23:33 -0500

    Adding menu-cleanup  by  username, tested with postman

--- menu-cleanup-service/src/main/java/com/olena/menucleanupservice/model/MenuCleanupRequestDTO.java
+++ menu-cleanup-service/src/main/java/com/olena/menucleanupservice/model/MenuCleanupRequestDTO.java
@@ -3,10 +3,12 @@ package com.olena.menucleanupservice.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class MenuCleanupRequestDTO {
 
     private String userId;
