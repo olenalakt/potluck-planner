ae75e8d3e1ef9ba14f0fcfbfba3e49fc90454853
Olena Laktyushkina <olaktyushkina@intercall.com>
2022-03-21 09:50:49 -0400

    Whiteboard code verification

--- user-service/src/main/java/com/olena/userservice/controller/UserController.java
+++ user-service/src/main/java/com/olena/userservice/controller/UserController.java
@@ -11,6 +11,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
 @Slf4j
 @RestController
 @RequestMapping(value = "/users")
@@ -87,5 +93,140 @@ public class UserController {
         } else {
             throw new BadInputException("deleteUserByUserName: userName can not be null");
         }
+    }
+
+    @RequestMapping(value = "/missingInt", method = RequestMethod.GET)
+    public ResponseEntity<?> getMissingInt(@RequestParam("intList") int[] intList) throws ServiceException, BadInputException {
+
+        Integer  result =  -1;
+
+        if (intList.length <= 0){
+            // exit if customerQuery is shorter then 2 chars
+            return ResponseEntity.ok(result);
+        }
+/*
+        intList.sort((a,b) -> a.compareTo(b));
+        log.debug("intList after sort:{}", intList);
+
+        // find missing -  O(N)
+        for (int i = 0; i < intList.size() - 1; i++) {
+            if (intList.get(i+1) - intList.get(i)  > 1) {
+                result =  intList.get(i) + 1;
+                break;
+            }
+        }
+*/
+
+        Arrays.sort(intList);
+        log.debug("intList after sort:{}", intList);
+
+        // find missing -  O(N)
+        for (int i = 0; i < intList.length - 1; i++) {
+            if (intList[i+1] - intList[i]  > 1) {
+                result = intList[i] + 1;
+                break;
+            }
+        }
+        if (intList[0] > 1) {
+            result = 1;
+        }
+
+
+/*        boolean isFound;
+        loop1:
+        for (int i = 1; i <= intList.size(); i++) {
+
+            isFound = false;
+            loop2:
+            for (int k = 0; k < intList.size(); k++) {
+                if (intList.get(k) == i) {
+                    isFound = true;
+                    break loop2;
+                }
+            }
+            if (!isFound) {
+                result = i;
+                break loop1;
+            }
+        }*/
+
+
+        return ResponseEntity.ok(result);
+    }
+
+    int binarySearch(int[] list, int value, int low, int high) {
+          int result = -1;
+
+          while (low <= high )  {
+              result =  ( low + high)  / 2;
+              int middle = list[result];
+              if (middle > value || middle ==  -1) {
+                  high =  result - 1;
+              } else if (middle < value) {
+                  low =  result + 1;
+              } else {
+                  break;
+              }
+          }
+
+          return result;
+    }
+
+    @RequestMapping(value = "/test/{searchString}", method = RequestMethod.GET)
+    public ResponseEntity<?> testSearch(@PathVariable("searchString") String customerQuery, @RequestParam("strlist") List<String> repository) throws ServiceException, BadInputException {
+
+     //   List<String> repository = Arrays.asList("mObile", "MOUSE", "moneypot", "moniTor", "mousepad");
+
+        // initialize result array
+        List<List<String>> result = new ArrayList<>();
+
+        if (customerQuery.length() < 2 || repository == null){
+            // exit if customerQuery is shorter then 2 chars
+            return ResponseEntity.ok(result);
+        }
+
+        // sort original list: O(N log N)
+        // convert to  lower case
+        List<String> repositoryLower = repository.stream().parallel().map(s -> s.toLowerCase()).collect(Collectors.toList());
+        log.debug("repository after map:{}", repositoryLower);
+
+        // sort original list
+        repositoryLower.sort((a,b) -> a.compareTo(b));
+        log.debug("repository after sort:{}", repositoryLower);
+
+        // initialize vars
+        StringBuffer keyString =  new StringBuffer();
+        keyString.append(customerQuery.substring(0, 2).toLowerCase());
+
+        List<String> matchingList = repositoryLower.stream().parallel().filter(s -> s.startsWith(keyString.toString())).collect(Collectors.toList());
+        List<String> tmpList = getTmpList(matchingList, 3);
+        result.add(tmpList);
+
+        //
+        for ( int i = 3; i <= customerQuery.length() && matchingList.size() != 0; i++) {
+
+            // ensure case is ignored
+            keyString.append(customerQuery.substring(i-1, i).toLowerCase());
+            matchingList = matchingList.stream().parallel().filter(s -> s.startsWith(keyString.toString())).collect(Collectors.toList());
+            if (matchingList.size() == 0) {
+                break;
+            }
+            log.debug("keyString:{}", keyString);
+            log.debug("i={}, matchingList:{}", i, matchingList);
+
+            tmpList = getTmpList(matchingList, 3);
+            result.add(tmpList);
+        }
+
+            return ResponseEntity.ok(result);
+    }
+
+    private List<String> getTmpList(List<String> matchingList, int maxTmpListSize) {
+
+        List<String> tmpList = new ArrayList<>();
+        for (int k = 0; k < maxTmpListSize && k < matchingList.size() ; k++){
+            tmpList.add(matchingList.get(k));
+        }
+        return tmpList;
     }
 }
--- user-service/src/main/java/com/olena/userservice/service/InitKafkaPropertiesService.java
+++ user-service/src/main/java/com/olena/userservice/service/InitKafkaPropertiesService.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Properties;
 
 /**
@@ -74,5 +75,142 @@ public class InitKafkaPropertiesService {
 
         return producerProperties;
     }
+
+/*
+    public int print_missing(int[] num) {
+
+        int n = num.length;
+        boolean isFound;
+
+        for (int i = 0; i <= n; i++) {
+            isFound = false;
+            for (int k = 0; k < n; k++) {
+                if (num[k] == i) {
+                    isFound = true;
+                    break;
+                }
+            }
+            if (!isFound) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int print_missing2(int[] num) {
+
+
+        int n = num.length;
+        int min_idx = 0;
+        int tmp;
+
+        // sort first -  O(N logN)
+        for (int i = 0; i < n - 1; i++) {
+
+            // Find the minimum element in unsorted array
+            min_idx = i;
+            for (int j = i + 1; j < n - 1; j++) {
+                if (num[j] < num[min_idx]) {
+                    min_idx = j;
+                }
+            }
+            // swap values
+            if (min_idx != i){
+                tmp = num[min_idx];
+                num[min_idx] = num[i];
+                num[i] = tmp;
+            }
+        }
+
+        // find missing -  O(N)
+        for (int i = 0; i < n - 2; i++) {
+            if (num[i+1] - num[i]  > 1) {
+                return num[i] + 1;
+            }
+        }
+
+        return -1;
+    }
+
+    @Test
+    void print_missingTest() {
+        int[] num1 = {3, 0, 1};
+        int[] num2 = {};
+
+        assertEquals(2, print_missing(num1));
+        assertEquals(-1, print_missing(num2));
+    }
+
+
+    private boolean check(char[] s, char d) {
+
+        for (int i = 0;  i < s.length; i++) {
+            if (s[i] ==  d) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+    private char[] sort(char[] s, int start_pos, int end_pos, char[] r) {
+
+        char tmp;
+        int min_pos;
+        char[] res = r;
+
+
+        for (int i = start_pos;  i < end_pos; i++) {
+            min_pos = i;
+            for (int j= i+1;  j < end_pos; j++) {
+                if (s[j] < s[min_pos]) {
+                    min_pos = j;
+                }
+            }
+
+            res[end_pos - start_pos - 1] = s[min_pos];
+        }
+
+        return res;
+    }
+
+    private boolean equal(char[] s, char[] d) {
+
+        for (int i = 0;  i < s.length; i++) {
+            if (s[i] !=  d[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public List<int> search_permutations(char[] b,  char[] s) {
+
+        // sort s
+        char[] ss =  sort(s, 0, s.length, s);
+        List<Integer> intList = new ArrayList<>();
+
+        for (int i = 0;  i < b.length; i++) {
+            if (check(s, b[i])) {
+                if ( equal ( ss, sort(b, i, i + s.length,for  ss)) {
+                    intList.add(i);
+                }
+            }
+        }
+
+    }
+
+    @Test
+    public void search_permutationsTest() {
+
+        char[] b = {'d','a','d','c','d','e','f','c','d','a','a','a','d','c','d'};
+        char[] s = {'a','d','c','d'};
+        List<Integer> intList = Arrays.asList(1, 2, 12);
+
+        assertEquals(intList, search_permutations(b, s));
+
+    }*/
 
 }
